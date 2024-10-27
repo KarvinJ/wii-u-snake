b45f5c05bb813e9c9351cb6c5e65578cf8fe2858
knight <karvin_jimenez@hotmail.com>
2024-10-27 14:08:18 -0400

    port finished. Doesn't have save and load high score support.

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,212 @@
 #include "sdl_starter.h"
 #include "sdl_assets_loader.h"
-#include <time.h>
 #include <unistd.h> // chdir header
 #include <romfs-wiiu.h>
 #include <whb/proc.h>
+#include <time.h>
+#include <deque>
+#include <math.h>
+#include <fstream>
 
 SDL_Window *window = nullptr;
 SDL_Renderer *renderer = nullptr;
 SDL_GameController *controller = nullptr;
 
-const int PLAYER_SPEED = 600;
-
-Sprite playerSprite;
-
-Mix_Music *music = nullptr;
-Mix_Chunk *sound = nullptr;
+Mix_Chunk *actionSound = nullptr;
 
 bool isGamePaused;
 
-SDL_Texture *pauseGameTexture = nullptr;
-SDL_Rect pauseGameBounds;
+int score;
+int highScore;
+
+SDL_Texture *pauseTexture = nullptr;
+SDL_Rect pauseBounds;
 
 SDL_Texture *scoreTexture = nullptr;
 SDL_Rect scoreBounds;
 
-int score;
+SDL_Texture *highScoreTexture = nullptr;
+SDL_Rect highScoreBounds;
 
 TTF_Font *font = nullptr;
 
-bool shouldCloseTheGame;
+typedef struct
+{
+    int x;
+    int y;
+} Vector2;
 
-SDL_Rect ball = {SCREEN_WIDTH / 2 + 50, SCREEN_HEIGHT / 2, 32, 32};
+typedef struct
+{
+    int cellCount;
+    int cellSize;
+    std::deque<Vector2> body;
+    Vector2 direction;
+    bool shouldAddSegment;
+} Snake;
 
-int ballVelocityX = 400;
-int ballVelocityY = 400;
+Snake snake;
 
-int colorIndex;
+typedef struct
+{
+    int cellCount;
+    int cellSize;
+    Vector2 position;
+    bool isDestroyed;
+} Food;
 
-SDL_Color colors[] = {
-    {128, 128, 128, 0}, // gray
-    {255, 255, 255, 0}, // white
-    {255, 0, 0, 0},     // red
-    {0, 255, 0, 0},     // green
-    {0, 0, 255, 0},     // blue
-    {255, 255, 0, 0},   // brown
-    {0, 255, 255, 0},   // cyan
-    {255, 0, 255, 0},   // purple
-};
+Food food;
+
+SDL_Rect foodBounds;
+
+int rand_range(int min, int max)
+{
+    return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+}
+
+Vector2 generateRandomPosition()
+{
+    int positionX = rand_range(0, CELL_COUNT - 1);
+    int positionY = rand_range(0, CELL_COUNT - 1);
+
+    return Vector2{positionX, positionY};
+}
+
+Vector2 vector2Add(Vector2 vector1, Vector2 vector2)
+{
+    Vector2 result = {vector1.x + vector2.x, vector1.y + vector2.y};
+
+    return result;
+}
+
+int vector2Equals(Vector2 vector1, Vector2 vector2)
+{
+    const float EPSILON = 0.000001f;
+    int result = ((fabsf(vector1.x - vector2.x)) <= (EPSILON * fmaxf(1.0f, fmaxf(fabsf(vector1.x), fabsf(vector2.x))))) &&
+                 ((fabsf(vector1.y - vector2.y)) <= (EPSILON * fmaxf(1.0f, fmaxf(fabsf(vector1.y), fabsf(vector2.y)))));
+
+    return result;
+}
+
+double lastUpdateTime = 0;
+
+bool eventTriggered(float deltaTime, float intervalUpdate)
+{
+    lastUpdateTime += deltaTime;
+
+    if (lastUpdateTime >= intervalUpdate)
+    {
+        lastUpdateTime = 0;
+
+        return true;
+    }
+
+    return false;
+}
+
+void saveScore()
+{
+    std::string path = "high-score.txt";
+
+    std::ofstream highScores(path);
+
+    std::string scoreString = std::to_string(score);
+    highScores << scoreString;
+
+    highScores.close();
+}
+
+int loadHighScore()
+{
+    std::string highScoreText;
+
+    std::string path = "high-score.txt";
+
+    std::ifstream highScores(path);
+
+    if (!highScores.is_open())
+    {
+        saveScore();
+
+        std::ifstream auxHighScores(path);
+
+        getline(auxHighScores, highScoreText);
+
+        highScores.close();
+
+        int highScore = stoi(highScoreText);
+
+        return highScore;
+    }
+
+    getline(highScores, highScoreText);
+
+    highScores.close();
+
+    int highScore = stoi(highScoreText);
+
+    return highScore;
+}
+
+void resetSnakePosition()
+{
+    // highScore = loadHighScore();
+
+    if (score > highScore)
+    {
+        saveScore();
+
+        std::string highScoreString = "High Score: " + std::to_string(score);
+
+        updateTextureText(highScoreTexture, highScoreString.c_str(), font, renderer);
+    }
+
+    snake.body = {{6, 9}, {5, 9}, {4, 9}};
+    snake.direction = {1, 0};
+
+    score = 0;
+    updateTextureText(scoreTexture, "Score: 0", font, renderer);
+}
+
+bool checkCollisionWithFood(Vector2 foodPosition)
+{
+    if (vector2Equals(snake.body[0], foodPosition))
+    {
+        snake.shouldAddSegment = true;
+        return true;
+    }
+
+    return false;
+}
+
+void checkCollisionWithEdges()
+{
+    if (snake.body[0].x == CELL_COUNT || snake.body[0].x == -1 || snake.body[0].y == CELL_COUNT || snake.body[0].y == -1)
+    {
+        resetSnakePosition();
+    }
+}
+
+void checkCollisionBetweenHeadAndBody()
+{
+    for (size_t i = 1; i < snake.body.size(); i++)
+    {
+        if (vector2Equals(snake.body[0], snake.body[i]))
+        {
+            resetSnakePosition();
+        }
+    }
+}
 
 void quitGame()
 {
+    Mix_FreeChunk(actionSound);
+    SDL_DestroyTexture(pauseTexture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    Mix_CloseAudio();
+    IMG_Quit();
+    TTF_Quit();
     SDL_Quit();
     WHBProcShutdown();
 }
@@ -64,86 +219,70 @@ void handleEvents()
     {
         if (event.type == SDL_QUIT)
         {
-            shouldCloseTheGame = true;
+            quitGame();
             break;
         }
 
-        if (event.type == SDL_JOYBUTTONDOWN)
+        if (event.type == SDL_CONTROLLERBUTTONDOWN && event.cbutton.button == SDL_CONTROLLER_BUTTON_START)
         {
-            if (event.jbutton.button == BUTTON_MINUS)
-            {
-                shouldCloseTheGame = true;
-                break;
-            }
-
-            if (event.jbutton.button == BUTTON_PLUS)
-            {
-                isGamePaused = !isGamePaused;
-                Mix_PlayChannel(-1, sound, 0);
-            }
+            isGamePaused = !isGamePaused;
+            Mix_PlayChannel(-1, actionSound, 0);
         }
     }
-}
-
-int rand_range(int min, int max)
-{
-    return min + rand() / (RAND_MAX / (max - min + 1) + 1);
 }
 
 void update(float deltaTime)
 {
-    if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_UP) && playerSprite.textureBounds.y > 0)
+    if (eventTriggered(deltaTime, 0.2))
     {
-        playerSprite.textureBounds.y -= PLAYER_SPEED * deltaTime;
+        if (!snake.shouldAddSegment)
+        {
+            snake.body.pop_back();
+            snake.body.push_front(vector2Add(snake.body[0], snake.direction));
+        }
+        else
+        {
+            snake.body.push_front(vector2Add(snake.body[0], snake.direction));
+            snake.shouldAddSegment = false;
+        }
     }
 
-    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_DOWN) && playerSprite.textureBounds.y < SCREEN_HEIGHT - playerSprite.textureBounds.h)
+    if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_UP) && snake.direction.y != 1)
     {
-        playerSprite.textureBounds.y += PLAYER_SPEED * deltaTime;
+        snake.direction = {0, -1};
     }
 
-    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_LEFT) && playerSprite.textureBounds.x > 0)
+    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_DOWN) && snake.direction.y != -1)
     {
-        playerSprite.textureBounds.x -= PLAYER_SPEED * deltaTime;
+        snake.direction = {0, 1};
     }
 
-    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_RIGHT) && playerSprite.textureBounds.x < SCREEN_WIDTH - playerSprite.textureBounds.w)
+    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_RIGHT) && snake.direction.x != -1)
     {
-        playerSprite.textureBounds.x += PLAYER_SPEED * deltaTime;
+        snake.direction = {1, 0};
     }
 
-    if (ball.x < 0 || ball.x > SCREEN_WIDTH - ball.w)
+    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_LEFT) && snake.direction.x != 1)
     {
-        ballVelocityX *= -1;
-
-        colorIndex = rand_range(0, 5);
+        snake.direction = {-1, 0};
     }
 
-    else if (ball.y < 0 || ball.y > SCREEN_HEIGHT - ball.h)
+    checkCollisionWithEdges();
+    checkCollisionBetweenHeadAndBody();
+
+    food.isDestroyed = checkCollisionWithFood(food.position);
+
+    if (food.isDestroyed)
     {
-        ballVelocityY *= -1;
-
-        colorIndex = rand_range(0, 5);
-    }
-
-    else if (SDL_HasIntersection(&playerSprite.textureBounds, &ball))
-    {
-        ballVelocityX *= -1;
-        ballVelocityY *= -1;
-
-        colorIndex = rand_range(0, 5);
-
-        Mix_PlayChannel(-1, sound, 0);
-
+        food.position = generateRandomPosition();
         score++;
 
-        std::string scoreString = "SCORE: " + std::to_string(score);
+        std::string scoreString = "Score: " + std::to_string(score);
 
         updateTextureText(scoreTexture, scoreString.c_str(), font, renderer);
-    }
 
-    ball.x += ballVelocityX * deltaTime;
-    ball.y += ballVelocityY * deltaTime;
+        Mix_PlayChannel(-1, actionSound, 0);
+    }
 }
 
 void renderSprite(Sprite &sprite)
@@ -156,21 +295,41 @@ void render()
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, colors[colorIndex].r, colors[colorIndex].g, colors[colorIndex].b, 255);
-
-    SDL_RenderFillRect(renderer, &ball);
-
-    renderSprite(playerSprite);
-
     if (isGamePaused)
     {
-        SDL_RenderCopy(renderer, pauseGameTexture, NULL, &pauseGameBounds);
+        SDL_RenderCopy(renderer, pauseTexture, NULL, &pauseBounds);
     }
 
     SDL_QueryTexture(scoreTexture, NULL, NULL, &scoreBounds.w, &scoreBounds.h);
-    scoreBounds.x = SCREEN_WIDTH / 2 - pauseGameBounds.w / 2;
+    scoreBounds.x = SCREEN_WIDTH / 2 + 100;
     scoreBounds.y = scoreBounds.h / 2;
     SDL_RenderCopy(renderer, scoreTexture, NULL, &scoreBounds);
+
+    SDL_QueryTexture(highScoreTexture, NULL, NULL, &highScoreBounds.w, &highScoreBounds.h);
+    highScoreBounds.x = 100;
+    highScoreBounds.y = highScoreBounds.h / 2;
+    SDL_RenderCopy(renderer, highScoreTexture, NULL, &highScoreBounds);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    for (size_t i = 0; i < snake.body.size(); i++)
+    {
+        int positionX = snake.body[i].x;
+        int positionY = snake.body[i].y;
+
+        SDL_Rect bodyBounds = {positionX * CELL_SIZE, positionY * CELL_SIZE, CELL_SIZE, CELL_SIZE};
+
+        SDL_RenderFillRect(renderer, &bodyBounds);
+    }
+
+    foodBounds = {food.position.x * CELL_SIZE, food.position.y * CELL_SIZE, CELL_SIZE, CELL_SIZE};
+
+    SDL_RenderFillRect(renderer, &foodBounds);
+
+    SDL_RenderDrawLine(renderer, 0, 1, SCREEN_WIDTH, 1);
+    SDL_RenderDrawLine(renderer, 0, SCREEN_HEIGHT - 1, SCREEN_WIDTH, SCREEN_HEIGHT - 1);
+    SDL_RenderDrawLine(renderer, 0, 0, 0, SCREEN_HEIGHT);
+    SDL_RenderDrawLine(renderer, SCREEN_WIDTH - 1, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT);
 
     SDL_RenderPresent(renderer);
 }
@@ -181,7 +340,7 @@ int main(int argc, char **argv)
     romfsInit();
     chdir("romfs:/");
 
-    window = SDL_CreateWindow("Wii U SDL Starter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     if (startSDL(window, renderer) > 0)
@@ -192,49 +351,44 @@ int main(int argc, char **argv)
     SDL_JoystickEventState(SDL_ENABLE);
     SDL_JoystickOpen(0);
 
-    if (SDL_NumJoysticks() < 1)
-    {
-        printf("No game controllers connected!\n");
-        return -1;
-    }
-    else
-    {
-        controller = SDL_GameControllerOpen(0);
-        if (controller == NULL)
-        {
-            printf("Unable to open game controller! SDL Error: %s\n", SDL_GetError());
-            return -1;
-        }
-    }
-
-    playerSprite = loadSprite(renderer, "sprites/alien_1.png", SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2);
+    controller = SDL_GameControllerOpen(0);
 
     font = TTF_OpenFont("fonts/LeroyLetteringLightBeta01.ttf", 36);
 
-    // render text as texture
-    updateTextureText(scoreTexture, "SCORE: 0", font, renderer);
+    updateTextureText(pauseTexture, "Game Paused", font, renderer);
 
-    updateTextureText(pauseGameTexture, "GAME PAUSED", font, renderer);
+    SDL_QueryTexture(pauseTexture, NULL, NULL, &pauseBounds.w, &pauseBounds.h);
+    pauseBounds.x = SCREEN_WIDTH / 2 - pauseBounds.w / 2;
+    pauseBounds.y = 100;
 
-    SDL_QueryTexture(pauseGameTexture, NULL, NULL, &pauseGameBounds.w, &pauseGameBounds.h);
-    pauseGameBounds.x = SCREEN_WIDTH / 2 - pauseGameBounds.w / 2;
-    pauseGameBounds.y = 200;
+    // highScore = loadHighScore();
 
-    // no need to keep the font loaded
-    // TTF_CloseFont(font);
+    std::string highScoreString = "High Score: " + std::to_string(highScore);
 
-    // load music and sounds from files
-    sound = loadSound("sounds/pop1.wav");
+    updateTextureText(highScoreTexture, highScoreString.c_str(), font, renderer);
 
-    music = loadMusic("music/background.ogg");
+    updateTextureText(scoreTexture, "Score: 0", font, renderer);
 
-    Mix_PlayMusic(music, -1);
+    actionSound = loadSound("sounds/pop1.wav");
+
+    Mix_VolumeChunk(actionSound, MIX_MAX_VOLUME / 2);
 
     Uint32 previousFrameTime = SDL_GetTicks();
     Uint32 currentFrameTime = previousFrameTime;
     float deltaTime = 0.0f;
 
-    while (!shouldCloseTheGame && WHBProcIsRunning())
+    srand(time(NULL));
+
+    Vector2 initialFoodPosition = generateRandomPosition();
+
+    food = {CELL_COUNT, CELL_SIZE, initialFoodPosition, false};
+
+    std::deque<Vector2> initialBody = {{6, 9}, {5, 9}, {4, 9}};
+    Vector2 direction = {1, 0};
+
+    snake = {CELL_COUNT, CELL_SIZE, initialBody, direction, false};
+
+    while (true)
     {
         currentFrameTime = SDL_GetTicks();
         deltaTime = (currentFrameTime - previousFrameTime) / 1000.0f;
--- src/sdl_starter.h
+++ src/sdl_starter.h
@@ -2,26 +2,10 @@
 
 #include <SDL2/SDL.h>
 
-// some switch buttons
-#define BUTTON_A 0
-#define BUTTON_B 1
-#define BUTTON_X 2
-#define BUTTON_Y 3
-#define BUTTON_STICKL 4
-#define BUTTON_STICKR 5
-#define BUTTON_L 6
-#define BUTTON_R 7
-#define BUTTON_ZL 8
-#define BUTTON_ZR 9
-#define BUTTON_PLUS 10
-#define BUTTON_MINUS 11
-#define BUTTON_LEFT 12
-#define BUTTON_UP 13
-#define BUTTON_RIGHT 14
-#define BUTTON_DOWN 15
-#define BUTTON_COUNT 16
+const int CELL_SIZE = 24;
+const int CELL_COUNT = 30;
 
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
+const int SCREEN_WIDTH = CELL_SIZE * CELL_COUNT;
+const int SCREEN_HEIGHT = CELL_SIZE * CELL_COUNT;
 
 int startSDL(SDL_Window *window, SDL_Renderer *renderer);
